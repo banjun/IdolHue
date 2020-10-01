f3cb67aae1a39b423a53a02ba95fbee3d6fc5ea4
banjun <banjun@gmail.com>
2020-10-01 22:32:57 +0900

    Initial Commit

---
+++ IdolHue/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  IdolHue
+//
+//  Created by BAN Jun on R 2/10/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
