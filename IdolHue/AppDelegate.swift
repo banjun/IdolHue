8b7e560ad6ebc815d70e7fff8cb790b9324b138f
banjun <banjun@gmail.com>
2020-10-03 01:08:32 +0900

    enable scroll wheel rotation and correct achromatic color translation

--- IdolHue/AppDelegate.swift
+++ IdolHue/AppDelegate.swift
@@ -13,12 +13,10 @@ import SwiftSparql
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow!
-    let idolsModel = Idols(idols: [])
+    let idolsModel = ContentView.Model()
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create the SwiftUI view that provides the window contents.
-        let contentView = ContentView(model: idolsModel)
-
         // Create the window and set the content view.
         window = NSWindow(
             contentRect: NSRect(x: 0, y: 0, width: 480, height: 300),
@@ -27,7 +25,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         window.isReleasedWhenClosed = false
         window.center()
         window.setFrameAutosaveName("Main Window")
-        window.contentView = NSHostingView(rootView: contentView)
+        window.contentView = HostingView(model: idolsModel)
         window.title = "Dye the sky in my hue"
         window.makeKeyAndOrderFront(nil)
 
@@ -44,11 +42,52 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         .onSuccess {(idols: [Idol]) in self.idolsModel.idols = idols}
         .onFailure {NSLog("%@", "query error: \(String(describing: $0))")}
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
-    }
-
-
 }
 
+struct Idol: Codable {
+    var name: String
+    var color: String
+
+    var hue: Float? {
+        guard color.count == 6,
+              let rgb = Int(color, radix: 16) else { return nil }
+        let r = Float((rgb & 0xff0000) >> 16) / 255
+        let g = Float((rgb & 0x00ff00) >> 8) / 255
+        let b = Float((rgb & 0x0000ff) >> 0) / 255
+        let max = Swift.max(r, g, b)
+        let min = Swift.min(r, g, b)
+        guard min < max else { return nil }
+        let h: Float = {
+            switch max {
+            case r: return (g - b) / (max - min) / 6 + (0 / 3)
+            case g: return (b - r) / (max - min) / 6 + (1 / 3)
+            case b: return (r - g) / (max - min) / 6 + (2 / 3)
+            default: fatalError()
+            }
+        }()
+        return h + (h < 0 ? 1 : 0)
+    }
+}
+
+final class HostingView: NSHostingView<ContentView> {
+    init(model: ContentView.Model) {
+        super.init(rootView: ContentView(model: model))
+    }
+
+    @objc required dynamic init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    required init(rootView: ContentView) {
+        fatalError("init(rootView:) has not been implemented")
+    }
+
+    override func hitTest(_ point: NSPoint) -> NSView? {
+        bounds.contains(point) ? self : super.hitTest(point)
+    }
+
+    override func scrollWheel(with event: NSEvent) {
+        guard case .scrollWheel = event.type else { return }
+        rootView.model.scrollOffset += Double(event.deltaY)
+    }
+}
--- IdolHue/ContentView.swift
+++ IdolHue/ContentView.swift
@@ -7,62 +7,35 @@
 
 import SwiftUI
 
-struct Idol: Codable {
-    var name: String
-    var color: String
-
-    var hue: Float? {
-        guard color.count == 6,
-              let rgb = Int(color, radix: 16) else { return nil }
-        let r = Float((rgb & 0xff0000) >> 16) / 255
-        let g = Float((rgb & 0x00ff00) >> 8) / 255
-        let b = Float((rgb & 0x0000ff) >> 0) / 255
-        let max = Swift.max(r, g, b)
-        let min = Swift.min(r, g, b)
-        guard min < max else { return nil }
-        let h: Float = {
-            switch max {
-            case r: return (g - b) / (max - min) / 6 + (0 / 3)
-            case g: return (b - r) / (max - min) / 6 + (1 / 3)
-            case b: return (r - g) / (max - min) / 6 + (2 / 3)
-            default: fatalError()
-            }
-        }()
-        return h + (h < 0 ? 1 : 0)
-    }
-}
-
-extension Idol: Identifiable {
-    var id: String { name }
-}
-
-class Idols: ObservableObject {
-    @Published var idols: [Idol]
-    init(idols: [Idol]) { self.idols = idols }
-}
-
 struct ContentView: View {
-    @ObservedObject var model: Idols
+    @ObservedObject var model: Model
+
+    class Model: ObservableObject {
+        @Published var idols: [Idol]
+        @Published var scrollOffset: Double = 0
+
+        init(idols: [Idol] = []) {self.idols = idols}
+    }
 
     var body: some View {
         GeometryReader { geometry in
             ZStack {
                 let side = min(geometry.size.width, geometry.size.height) / 3
-                ForEach(Array(stride(from: 0, to: 360, by: 1)), id: \.self) { hue in Rectangle()
+                ForEach([CGFloat](stride(from: 0, to: 360, by: 1)), id: \.self) { hue in Rectangle()
                     .foregroundColor(.init(hue: Double(hue) / 360, saturation: 1, brightness: 1))
                     .frame(width: 32, height: 4)
                     .transformEffect(
                         CGAffineTransform.identity
                             .translatedBy(x: +16, y: +2)
-                            .rotated(by: hue / 180 * .pi)
+                            .rotated(by: (CGFloat(model.scrollOffset) + hue) / 180 * .pi)
                             .translatedBy(x: side, y: 0)
                             .translatedBy(x: -16, y: -2))
                 }
-                ForEach(model.idols) { idol in
+                ForEach(model.idols, id: \.name) { idol in
                     Text(idol.name)
-                        .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 1, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/)
-                        .transformEffect(.init(translationX: side, y: 0))
-                        .rotationEffect(Angle(degrees: 360 * Double(idol.hue ?? 0)), anchor: .center)
+                        .shadow(color: .init(.textBackgroundColor), radius: 1)
+                        .transformEffect(.init(translationX: idol.hue != nil ? side : 0, y: 0))
+                        .rotationEffect(Angle(degrees: idol.hue.map {model.scrollOffset + 360 * Double($0)} ?? 0), anchor: .center)
                 }
             }.frame(width: geometry.size.width, height: geometry.size.height)
         }
@@ -76,7 +49,8 @@ struct ContentView_Previews: PreviewProvider {
             Idol(name: "橘ありす", color: "5881C1"),
             Idol(name: "橘ありすR", color: "FF0000"),
             Idol(name: "橘ありすG", color: "00FF00"),
-            Idol(name: "橘ありすB", color: "0000FF")
+            Idol(name: "橘ありすB", color: "0000FF"),
+            Idol(name: "橘ありすW", color: "FFFFFF")
         ]))
     }
 }
